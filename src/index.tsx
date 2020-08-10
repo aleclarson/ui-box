b40334e319f64b3262743d8af0ab80fa4de07320
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-08-10 16:46:52 -0400

    fix: stop using default export
    BREAKING CHANGE

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as cache from './cache'
 import * as styles from './styles'
 
-export { default } from './box'
+export { default as Box } from './box'
 export { default as splitProps } from './utils/split-props'
 export { default as splitBoxProps } from './utils/split-box-props'
 export { setClassNamePrefix } from './get-class-name'
